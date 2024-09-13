5003582f2a70ecd3d98f3c02ce48c89a4db79975
Manta-Rae <Cronin364@wearescpps.org>
2024-09-13 18:39:32 +0000

    push again

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -38,7 +38,19 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
+    createPlatform(1000, 620, 10, 30)
+    createPlatform(800, 500, 20, 10)
+    createPlatform(500, 475, 50, 30)
+    createPlatform(400, 345, 20, 10)
+    createPlatform(550, 265, 10, 10)
+    createPlatform(800, 300, 40, 20)
+    createPlatform(1100, 245, 50, .1)
+    createPlatform(975, 100, .001, 200)
+    createPlatform(975, 450, 30, .1)
+    createPlatform(1200, 370, 40, .5)
+    createPlatform(425, 150, 30, .5)
+    createPlatform(200, 225, 40, .2)
+    createPlatform(200, 450, 40, .5)
 
 
     
@@ -47,7 +59,9 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
+    createCollectable('database', 1100, 185)
+    createCollectable('grace', 200, 180)
+    createCollectable('max', 200, 405)
 
 
     
@@ -56,7 +70,9 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+    createCannon('top', 975, 1000, 30, 30)
+    createCannon('left', 575, 800, 20, 20)
+    createCannon('right', 125, 900, 10, 10)
 
 
     
